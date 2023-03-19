178e98d822a3cbb1579c9cbfa5005e0fd1a64ee3
marovichn <nikolamarovic.11@gmail.com>
2023-03-19 17:10:00 +0100

    simple-testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "./components/Button/Button";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  const [showP, setShowP] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+
+  const handler = () => {
+    setShowP((prev) => {
+      return !showP;
+    }); /* 
+    setDisabled(true); */
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showP && <p>This is new</p>}
+      <Button onClick={handler} disabled={disabled}>
+        {showP ? "Remove" : "Add"} paragraph
+      </Button>
     </div>
   );
 }
