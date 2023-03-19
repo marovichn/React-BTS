3db6c252d482902a25411771faaa9c319e4648fe
marovichn <nikolamarovic.11@gmail.com>
2023-03-19 19:06:55 +0100

    testing-with-other-functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Button from "./components/Button/Button";
 
 import "./App.css";
@@ -6,20 +6,33 @@ import DemoOutput from "./components/Button/Demo/DemoOutput";
 
 function App() {
   const [showP, setShowP] = useState(false);
+  const [allowP, setAllowP] = useState(false);
 
   console.log("app running");
 
-  const handler = () => {
-    setShowP((prev) => {
-      return !showP;
-    });
+  const handler = useCallback(() => {
+    if (allowP) {
+      setShowP((prev) => !showP);
+    }
+  }, [showP, allowP]);
+
+  const allowToggle = () => {
+    setAllowP(true);
+  };
+
+  const disAllowToggle = () => {
+    setAllowP(false);
   };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showP}></DemoOutput>
-      <Button onClick={handler}>{showP ? "Remove" : "Add"} paragraph</Button>
+      <Button onClick={allowToggle}>Allow paragraph toggling</Button>
+      <Button onClick={handler} disabled={!allowP}>
+        {showP ? "Remove" : "Add"} paragraph{" "}
+      </Button>
+      <Button onClick={disAllowToggle}>Disable paragraph toggling</Button>
     </div>
   );
 }
